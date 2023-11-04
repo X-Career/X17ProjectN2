ed85a30467e8e4177bdf31680a043e55e2901565
Phạm Đức Tài <116778505+dreamlaptrinh@users.noreply.github.com>
2023-11-04 16:18:28 +0700

    validate email
    Tai Pham

--- frontend/src/component/admin/mailMgr.js
+++ frontend/src/component/admin/mailMgr.js
@@ -1,55 +1,45 @@
 import React, { useState } from 'react';
-import { Button, Layout, theme, message, Modal} from 'antd';
+import { Button, Layout, theme, message, Modal, Form, Item } from 'antd';
 import "./Admin.css"
 import { sendMail } from "../../services/mail"
+import { useForm } from 'antd/es/form/Form';
+import Input from 'antd/es/input/Input';
 
 
 const { Content } = Layout;
 
 
 const MailMgr = () => {
-    const [emailCand, setEmailCand] = useState("")
-    const [nameCand, setNameCand] = useState("")
-    const [intro, setIntro] = useState("Please complete this test")
-    const [link, setLink] = useState("")
-    const [outro, setOutro] = useState("Thanks for doing this test")
     const [modaOpen, setModaOpen] = useState(false);
-
-
-
- 
-    const confirm = async (e) => { 
+    const [form] = useForm();
+    const confirm = async (e) => {
         e.preventDefault();
         try {
-            if (!emailCand || !nameCand || !intro || !link || !outro) {
-                setTimeout(() => {
-                    message.error('fail to send mail')
-                }, 3000);
-            } else {
-                const Data = {
-                    userEmail: emailCand,
-                    candidateName: nameCand,
-                    intro: intro,
-                    linkTest: link,
-                    outro: outro,
+            const formData = await form.validateFields();
+            let values = Object.entries(formData).reduce((acc, [key, value]) => {
+                if (value !== '') {
+                    acc[key] = value;
                 }
-                const { emailtoCandidate } = await sendMail(Data)
-                console.log(emailtoCandidate)
-                setModaOpen(false);   
+                return acc;
+            }, {});
+            console.log(values);
+                const res = await sendMail(values)
+                console.log(res)
+                setModaOpen(false);
                 setTimeout(() => {
                     message.success('Mail has been sent')
                 }, 3000);
-            }
+            
         } catch (error) {
             console.error("Đã xảy ra lỗi khi gửi email:", error);
         }
-      };
-      console.log(confirm)
+    };
+    console.log(confirm)
     const cancel = (e) => {
         console.log(e);
         setModaOpen(false);
         message.error('Rejected send');
-      };
+    };
 
     const {
         token: { colorBgContainer },
@@ -63,71 +53,76 @@ const MailMgr = () => {
                 background: colorBgContainer,
             }}
         >
-            <div style={{height:"100vh"}}>
+            <div style={{ height: "100vh" }}>
                 <div className='Tittle'>Mail to Candidate</div>
                 <div>
-                    <div className='Profile_Container' style={{ width: "80%", margin: "auto" }}>
-                        <form style={{ width: "100%" }}>
-                            <div className="Tittle_Element">Candidate Email:</div>
-                            <input
-                                className='TE_Input'
-                                type="email"
-                                value={emailCand}
+                    {/* <div className='Profile_Container' style={{ width: "80%", margin: "auto" }}> */}
+                    <Form
+                        style={{ width: "100%" }}
+                        layout="vertical"
+                        form={form}
+                        className="form_send_mail"
+                    >
+                        <Form.Item
+                            name="userEmail"
+                            label="Candidate Email" 
+                            rules={[{ required: true, message: "Please enter Candidate Email!" }]}
+                        >
+                            <Input
                                 placeholder="Please enter Candidate Email"
-                                onChange={(e) => setEmailCand(e.target.value)}
                             />
-                            <hr />
-                            <div className="Tittle_Element">Candidate Name:</div>
-                            <input
-                                className='TE_Input'
-                                type="text"
-                                value={nameCand}
-                                placeholder="Please enter Candidate name"
-                                onChange={(e) => setNameCand(e.target.value)}
+                        </Form.Item>
+                        <Form.Item
+                            name="candidateName"
+                            label="Candidate Name" 
+                            rules={[{ required: true, message: "Please enter Candidate Name!" }]}
+                        >
+                            <Input
+                                placeholder="Please enter Candidate Name"
                             />
-                            <hr />
-                            <div className="Tittle_Element">Intro:</div>
-                            <input
-                                className='TE_Input'
-                                type="text"
-                                value={intro}
-                                placeholder="Please enter Intro."
-                                onChange={(e) => setIntro(e.target.value)}
+                        </Form.Item>
+                        <Form.Item
+                            name="intro"
+                            label="Intro" 
+                            rules={[{ required: true, message: "Please enter Intro!" }]}
+                        >
+                            <Input
+                                placeholder="Please enter Intro"
                             />
-                            <hr />
-                            <div className="Tittle_Element">Link test:</div>
-                            <input
-                                className='TE_Input'
-                                type="text"
-                                value={link}
+                        </Form.Item>
+                        <Form.Item
+                            name="linkTest"
+                            label="Link Test" 
+                            rules={[{ required: true, message: "Please enter Link Test!" }]}
+                        >
+                            <Input
                                 placeholder="Please enter Link Test"
-                                onChange={(e) => setLink(e.target.value)}
                             />
-                            <hr />
-                            <div className="Tittle_Element">Outro:</div>
-                            <input
-                                className='TE_Input'
-                                type="text"
-                                value={outro}
+                        </Form.Item>
+                        <Form.Item
+                            name="outro"
+                            label="Outro" 
+                            rules={[{ required: true, message: "Please enter Outro!" }]}
+                        >
+                            <Input
                                 placeholder="Please enter Outro"
-                                onChange={(e) => setOutro(e.target.value)}
                             />
-                            <hr />
-                            <Button className="Tittle_Element" style={{ height: "40px", marginTop: "12px" }} type='primary' shape='round'
-                            onClick={()=>setModaOpen(true)}
-                                >Send Mail</Button>
-                            <Modal 
-                                title="Send mail to Candidate"
-                                centered
-                                open={modaOpen}
-                                onOk={confirm}
-                                onCancel={cancel}
-                                okText="Confirm" 
-                                cancelText="Cancel">
-                                <p>Are you sure to send this mail?</p>
-                            </Modal>
-                        </form>
-                    </div>
+                        </Form.Item>
+                        <Button className="Tittle_Element" style={{ height: "40px", marginTop: "12px" }} type='primary' shape='round'
+                            onClick={() => setModaOpen(true)}
+                        >Send Mail</Button>
+                        <Modal
+                            title="Send mail to Candidate"
+                            centered
+                            open={modaOpen}
+                            onOk={confirm}
+                            onCancel={cancel}
+                            okText="Confirm"
+                            cancelText="Cancel">
+                            <p>Are you sure to send this mail?</p>
+                        </Modal>
+                    </Form>
+                    {/* </div> */}
                 </div>
             </div>
         </Content>
